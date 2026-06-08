ccb5558622b6dceab5455b58e18a8f6cc5972b6d
Albert Marshall <marshall.k.albert@gmail.com>
2026-06-08 17:57:33 -0500

    Added Memory Usage

--- Server Performace Stats/server-stats.sh
+++ Server Performace Stats/server-stats.sh
@@ -4,3 +4,6 @@ echo "--Total CPU Usage--"
 top -bn1 | grep "%CPU" | awk '{print "Usage: "100-$1 "%"}'
 
 echo 
+
+echo "--Total Memory Usage--"
+free | grep "Mem" -w | awk '{printf "Total: %.2fGi / Used: %.2fGi (%.2f%%) / Free: %.2fGi (%.2f%%)\n", $2/1024^2, $3/1024^2, $3/$2*100, $4/1024^2, $4/$2*100}'
